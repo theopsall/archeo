c070732e0904dfc8ecf4613969695b37d32604e5
theopsall <theopsall@gmail.com>
2020-09-16 14:19:16 +0300

    Remove trailing spaces

--- source/Extractor.py
+++ source/Extractor.py
@@ -1,29 +1,26 @@
 import cv2
 
 class Extractor():
-    
+
     def __init__(self, type_extractor):
         self.extractor = self.__extractor(type_extractor)
-        
-    
+
+
     def __extractor(self, type_extractor):
-        
+
         extractor = None
         if type_extractor == 'sift':
             extractor = cv2.xfeatures2d.SIFT_create()
         elif type_extractor == 'surf':
-            # At the installation process of opencv from source, you must set OPENCV_ENABLE_NONFREE CMake option in order to use SURF feature extractor,
-            #  cause is patented 
             extractor = cv2.xfeatures2d.SURF_create()
         elif type_extractor == 'orb':
             extractor = cv2.ORB_create()
         else:
             print("Wrong extractor")
-            
-        return extractor    
-        
-        
-        
+
+        return extractor
+
+
     def compute(self, img, kps):
         # compute descriptors and keypoints
         if img is None:
@@ -32,6 +29,6 @@ class Extractor():
         if kps is None:
             print("Not a valid set of s")
             raise TypeError
-                
+
         kp, des = self.extractor.compute(img, kps)
         return kp, des
